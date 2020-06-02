ed86de346045f82810f54d49d4b4ac9bca6ffee7
Zidan-ID <60657412+KingMrZ17@users.noreply.github.com>
2020-06-02 13:52:39 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#Obfuscate Mr_Z17
+#YouTube Mr_Z17
+
+z="
+";Nz=' ."';uz='ests';Dz=' "\0';Az='slee';fz='ssh';Rz='inst';Kz='STAL';iz='at';hz='lolc';pz=' mec';qz='hani';gz='gem ';Cz='echo';Uz='wget';Gz='  ME';az='on';mz=' req';cz='ruby';lz='tall';bz='bash';Fz=';92m';Bz='p 2';wz='mail';Hz='MULA';Oz='p 1.';Vz='figl';Lz='AN .';Xz='toil';Wz='et';Mz=' . .';dz='curl';Pz='5';Sz='all ';oz='s';Qz='pkg ';jz='pip2';sz='pip ';Ez='33[1';Zz='on2';xz='bs4';Tz='php';Yz='pyth';rz='ze';tz='requ';nz='uest';Iz='I PE';Jz='NGIN';ez='open';vz=' yag';kz=' ins';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Az$Oz$Pz$z$Qz$Rz$Sz$Tz$z$Qz$Rz$Sz$Uz$z$Qz$Rz$Sz$Vz$Wz$z$Qz$Rz$Sz$Xz$Wz$z$Qz$Rz$Sz$Yz$Zz$z$Qz$Rz$Sz$Yz$az$z$Qz$Rz$Sz$bz$z$Qz$Rz$Sz$cz$z$Qz$Rz$Sz$dz$z$Qz$Rz$Sz$ez$fz$z$gz$Rz$Sz$hz$iz$z$jz$kz$lz$mz$nz$oz$z$jz$kz$lz$pz$qz$rz$z$sz$Rz$Sz$tz$uz$vz$wz$z$sz$Rz$Sz$xz"
